5380f725f89893d0a17cb09a5176b56bd7726a51
AntonMolchanov <a.molchanov886@gmail.com>
2022-03-03 23:06:21 +0300

    task 13

--- C#_2/Program.cs
+++ C#_2/Program.cs
@@ -5,22 +5,55 @@
 782 -> 8
 918 -> 1
 */
-
-int secNum(int number)
+/*
+int noSecond(int number)
 {
-    if (number > 99 && number < 1000)
+    if(number > 99 && number < 1000)
     {
         int ost = number % 100;
-        return ost /10;
+        return ost / 10;
     }
     else
     {
-        Console.WriteLine("Ваше число не подходит под условие задачи.");
+        return -1;
     }
 }
 
 Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Вторая цифра в введённом числе: " + secNum);
+Console.WriteLine("Вторая цифра в введённом числе: " + noSecond(num));
+*/
 
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+int thirdNum(int number)
+{
+    if(number > 999)
+    {
+        while(number > 999)
+        {
+            number = number / 10;
+        }
+    }
+    return number % 10;
+}
+
+Console.WriteLine("Введите любое число ");
+int result = Convert.ToInt32(Console.ReadLine());
+if(result < 100)
+    {
+        Console.WriteLine("Третьей цифры нет.");
+    }
+else
+{
+    Console.WriteLine("Третья цифра в числе " + result  + " является " + thirdNum(result));
+}
