7ee9b3707a800c637331d822cca1f7564f81168b
AntonMolchanov <a.molchanov886@gmail.com>
2022-03-27 01:34:48 +0300

    2 task

--- C#_4/Program.cs
+++ C#_4/Program.cs
@@ -4,7 +4,7 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
-
+/*
 double exponent(double A, double B)
 {
     if (B > 0)
@@ -31,9 +31,12 @@ double exponent(double A, double B)
     }
 }
 
+Console.WriteLine("Введите число, которое необходимо возвести в степень ");
 double a1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую неоюходимо возвести число ");
 double b1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Число " + a1 + " в степени " + b1 + " равно " + exponent(a1, b1));
+*/
 
 /*
 Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -41,6 +44,23 @@ Console.WriteLine("Число " + a1 + " в степени " + b1 + " равно
 452 -> 11
 82 -> 10
 9012 -> 12
+*/
+/*
+int numSum(int num)
+{
+    int sum = 0;
+    int num1 = 1;
+    while (num1 > 0)
+    {
+        num1 = num % 10;
+        sum = sum + num1;
+        num = num / 10;
+    }
+    return sum;
+}
+Console.WriteLine("Введите любое число ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Сумма цифр в числе равна " + numSum(number));
 */
 
 /*
